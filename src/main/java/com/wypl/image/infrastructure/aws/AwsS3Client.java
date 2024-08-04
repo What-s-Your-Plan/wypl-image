0652715dd116b2487dfbbac6be236dc044541be7
KIMSEI1124 <workju1124@gmail.com>
2024-08-04 12:59:25 +0900

    Feat(#1): S3 업로드 기능 구현

---
+++ src/main/java/com/wypl/image/infrastructure/aws/AwsS3Client.java
@@ -0,0 +1,27 @@
+package com.wypl.image.infrastructure.aws;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.wypl.image.infrastructure.ImageUploadable;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class AwsS3Client implements ImageUploadable {
+
+	private final AmazonS3Client amazonS3Client;
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	@Override
+	public String imageUpload(final File file) {
+		amazonS3Client.putObject(bucket, file.getName(), file);
+		return amazonS3Client.getUrl(bucket, file.getName()).toString();
+	}
+}
