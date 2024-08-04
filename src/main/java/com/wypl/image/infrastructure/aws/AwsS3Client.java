fa4ef85e702d3dfb5f4ceaf92e7cc75cdd0b81e5
KIMSEI1124 <workju1124@gmail.com>
2024-08-04 18:57:26 +0900

    Refactor(#1): AwsS3Client 주석 작성

--- src/main/java/com/wypl/image/infrastructure/aws/AwsS3Client.java
+++ src/main/java/com/wypl/image/infrastructure/aws/AwsS3Client.java
@@ -20,6 +20,12 @@ public class AwsS3Client implements ImageUploadable {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
+	/**
+	 * 파일을 <a href="https://aws.amazon.com/ko/s3/">AWS S3</a>에 업로드한 후 업로드된 파일의 경로를 반환합니다.
+	 *
+	 * @param file AWS S3에 업로드할 파일
+	 * @return AWS S3에 업로드된 파일의 경로
+	 */
 	@Override
 	public String imageUpload(final File file) {
 		amazonS3Client.putObject(bucket, file.getName(), file);
