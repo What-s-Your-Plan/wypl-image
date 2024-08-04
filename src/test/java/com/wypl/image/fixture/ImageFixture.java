f09af73a02bf2bd2d064da8d38954d86915b9347
KIMSEI1124 <workju1124@gmail.com>
2024-08-04 21:36:53 +0900

    Test(#5): 이미지 확장자 테스트 추가
    avif, gif 확장자 테스트 추가

--- src/test/java/com/wypl/image/fixture/ImageFixture.java
+++ src/test/java/com/wypl/image/fixture/ImageFixture.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import org.springframework.mock.web.MockMultipartFile;
 
 public enum ImageFixture {
-	PNG_IMAGE("image", "png", "image/png", "src/test/resources/image/image.png"),
+	AVIF_IMAGE("image", "avif", "image/avif", "src/test/resources/image/image.avif"),
+	GIF_IMAGE("image", "gif", "image/gif", "src/test/resources/image/image.gif"),
 	JPG_IMAGE("image", "jpg", "image/jpg", "src/test/resources/image/image.jpg"),
-	JPEG_IMAGE("image", "jpeg", "image/jpeg", "src/test/resources/image/image.jpeg");
+	JPEG_IMAGE("image", "jpeg", "image/jpeg", "src/test/resources/image/image.jpeg"),
+	PNG_IMAGE("image", "png", "image/png", "src/test/resources/image/image.png");
 
 	private final String name;
 	private final String contentType;
