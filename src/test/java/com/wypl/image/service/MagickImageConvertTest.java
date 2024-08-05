ea9936a28bb295fa9c2e6ff6356026620c60fbc7
KIMSEI1124 <workju1124@gmail.com>
2024-08-05 11:35:05 +0900

    ### 제목 # 커밋 타입: 작업내용 (제목과 본문은 한 줄 띄워주세요) Feat(#5): 테스트 이미지 파일 삭제 기능 구현
    ### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    # 무엇을, 왜, 어떻게 했는지
    
    ### 꼬리말
    # (선택) 이슈번호 작성
    
    #   [커밋 타입]
    #   feat       : 기능 (새로운 기능)
    #   fix        : 버그 (버그 수정)
    #   design     : CSS 등 사용자 UI 디자인 변경
    #   refactor   : 리팩토링
    #   style      : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs       : 문서 (문서 추가, 수정, 삭제)
    #   test       : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore      : 기타 변경사항 (빌드 스크립트 수정 등)
    #   rename     : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove     : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   [이슈 타입]
    #   fixed      : 이슈 수정중 ( 아직 해결되지 않은 경우 )
    #   Resolves   : 이슈룰 해결했을 때 사용
    #   Ref        : 참고할 이슈가 있을 때 사용
    #   Related to : 해당 커밋에 관련된 이슈번호 ( 아직 해결되지 않은 경우 )
    # ------------------
    #   [체크리스트]
    #   제목 첫 글자는 대문자로 작성했나요?
    #   제목은 명령문으로 작성했나요?
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기

--- src/test/java/com/wypl/image/service/MagickImageConvertTest.java
+++ src/test/java/com/wypl/image/service/MagickImageConvertTest.java
@@ -1,6 +1,9 @@
 package com.wypl.image.service;
 
+import java.io.File;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.wypl.image.fixture.ImageFixture;
+import com.wypl.image.utils.ImageRemoveUtils;
 
 @SpringBootTest
 class MagickImageConvertTest {
@@ -15,11 +19,19 @@ class MagickImageConvertTest {
 	@Autowired
 	private MagickImageConvert magickImageConvert;
 
+	private File avifImageFile;
+
+	@AfterEach
+	void removeTestImageFile() {
+		ImageRemoveUtils.removeImages(avifImageFile);
+	}
+
 	@DisplayName("MagickImage 를 사용하여 이미지를 변환 및 압축한다.")
 	@ParameterizedTest
 	@EnumSource(ImageFixture.class)
 	void imageTest(ImageFixture fixture) {
-		Assertions.assertThatCode(() -> magickImageConvert.imageConvert(fixture.getMockMultipartFile()))
+		Assertions.assertThatCode(
+						() -> avifImageFile = magickImageConvert.imageConvert(fixture.getMockMultipartFile()))
 				.doesNotThrowAnyException();
 	}
 }
